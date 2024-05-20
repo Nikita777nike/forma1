e6dbeb68dd8747071c0abb9d9530c5e946e3d7d2
Nikita777nike <sem.nike@yandex.ru>
2024-05-20 12:02:15 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+number_of_tasks = 12
+number_of_hours = 1.5
+name_of_course = "Python"
+time_for_one_task = number_of_hours/number_of_tasks
+print("Курс: " + name_of_course +
+", всего задач: " + str(number_of_tasks) +
+", затрачено часов: " + str(number_of_hours) +
+", среднее время выполнения: " + str(time_for_one_task) + ".")
