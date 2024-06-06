9a7ad4c341cc5833ab217baa57b2c70f7f12f0f3
Nikita777nike <sem.nike@yandex.ru>
2024-06-06 21:58:41 +0300

    Add files via upload

---
+++ module_2_7.py
@@ -0,0 +1,13 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    if len(str_number) > 1:
+        first = int(str_number[0])
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return int(str_number)
+
+result = get_multiplied_digits(40203)
+print(result)
+
+
+
