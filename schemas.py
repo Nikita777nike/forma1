e1a1fc9d3d01653021e01be88b595a429c9d686b
Nikita777nike <sem.nike@yandex.ru>
2024-11-08 11:23:41 +0300

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+class CreateUser(BaseModel):
+    username: str
+    firstname: str
+    lastname: str
+    age: int
+
+class UpdateUser(BaseModel):
+    username: str
+    firstname: str
+    lastname: str
+    age: int
+
+
+class CreateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
+
+
+class UpdateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
